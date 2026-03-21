007eefdbe5e59c539d5aba733b3e32d3d85f9cd5
Alokesh13 <adpvlogs7@gmail.com>
2026-03-21 16:17:21 +0530

    Update and rename supabase.ts to utils.ts

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js"
-
-const supabaseUrl = "https://egeefulckusdepssgykk.supabase.co"
-const supabaseKey = "sb_publishable_6IxH5R3CbqzqrwawozSzvQ_DlTdXxS9"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
